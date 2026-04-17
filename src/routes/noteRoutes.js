3080b7cc75701d1afae90586ee8281ebd532454d
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-17 15:10:30 +0530

    feat: add create note endpoint (POST /api/notes)

---
+++ src/routes/noteRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createNote } from "../controllers/noteController.js";
+
+const router = Router();
+
+router.post("/", createNote);
+
+export default router;
